1ed03ee38b9002e8062db9075092fe96729001d1
Violeta Mediavilla <violeta.alicia@hotmail.com>
2023-04-03 16:35:13 -0500

    solved!

--- Exercise 1/exercise.py
+++ Exercise 1/exercise.py
@@ -2,8 +2,25 @@
 
 
 def sonar(filename):
-    # TODO
-    return 0
+    measurements = []
+    file_content = []
+    large_measurements_list = []
+    largest_measurement = 0
+    count = 0
+
+    with open(filename) as file:
+        file_content = file.readlines()
+
+    for element in file_content:
+        measurements.append(int(element))
+
+    while count < len(measurements) - 1:
+        if measurements[count + 1] > measurements[count]:
+            largest_measurement = measurements[count + 1]
+            large_measurements_list.append(largest_measurement)
+        count += 1
+
+    return len(large_measurements_list)
 
 
 def main():
@@ -16,7 +33,7 @@ def main():
             print(f"Correct result for case {i + 1}: {result}")
         else:
             print(f"Incorrect result for case {i + 1}: {expected} is " \
-                f"expected, but {result} is returned")
+                  f"expected, but {result} is returned")
 
 
 if __name__ == "__main__":
